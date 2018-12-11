c2f917dc9e7cf6439c61e166354456db8c1673fb
Max Osborne <BurntAluminum@gmail.com>
2018-12-11 14:07:21 -0800

    This commit includes my JQuery intro

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a damn number"));
+var number2 = parseInt(prompt("Enter another damn number"));
+var result = add(number1, number2);
+alert(result);
